24bd087229055a527b5a33c7989ad5423b17de1d
BogdanZarichnyy <bogdanzarichnyy@gmail.com>
2025-09-23 03:38:17 +0300

    fix build

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -4,10 +4,11 @@ interface Props {
   className?: string
 }
 
-export default function Dashboard({ children }: Readonly<{children: React.ReactNode}>) {
+export default function Dashboard({ children }: { children: React.ReactNode }) {
   return (
     <div className={cn("", "")}>
       Dashboard
+      {children}
     </div>
   );
 }
--- app/(root)/@modal/(.)product/[id]/page.tsx
+++ app/(root)/@modal/(.)product/[id]/page.tsx
@@ -6,8 +6,9 @@ interface Props {
   params: { id: string };
 }
 
-export default async function ProductModalPage({ params}: Props) {
+export default async function ProductModalPage({ params }: Props) {
   const awaitedParams = await params; // ⚠ обов'язково await
+  // const awaitedParams = params; // без await
   const id = awaitedParams.id;
 
   const product = await prisma.product.findFirst({ 
